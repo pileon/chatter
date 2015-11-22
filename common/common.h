e4ed8457b283b75217cf6977ba07b61b5a01bf6e
Joachim Pileborg <arrow@pileborg.se>
2015-11-22 14:39:55 +0100

    Added common library

---
+++ common/common.h
@@ -0,0 +1,6 @@
+#ifndef COMMON_COMMON_H
+#define COMMON_COMMON_H
+
+void hello();
+
+#endif // COMMON_COMMON_H
--- server/server.cpp
+++ server/server.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "../common/common.h"
 
 int main()
 {
     std::cout << "Hello from the server\n";
+    hello();
 }
